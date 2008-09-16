4aa29264bf63e47646926da92502d5c8524b1c50
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2008-09-16 15:19:10 +0000

    Added external test for boost.mpi.

---
+++ test/external/boost.mpi/pt2pt.cpp
@@ -0,0 +1,37 @@
+#include <boost/mpi.hpp>
+#include <iostream>
+#include <boost/serialization/string.hpp>
+
+namespace mpi = boost::mpi;
+
+int main(int argc, char* argv[]) 
+
+{
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+
+  int TAG_0 = 0;
+  int TAG_1 = 1;
+
+  if (world.rank() == 0) {
+
+    int myPartnerRank = 1;
+
+    world.send(myPartnerRank, TAG_0, std::string("Hello"));
+    std::string msg;
+    world.recv(myPartnerRank, TAG_1, msg);
+    std::cout << msg << "!" << std::endl;
+  } else {
+ 
+    int myPartnerRank = 0;
+
+    std::string msg;
+    world.recv(myPartnerRank, TAG_0, msg);
+    std::cout << msg << ", ";
+    std::cout.flush();
+    world.send(myPartnerRank, TAG_1, std::string("world"));
+  }
+
+  return 0;
+}
+
