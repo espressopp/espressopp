b76ea864cfa3f3225c6dabb289377393bb7c3f78
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2008-10-02 14:35:39 +0000

    missing...

---
+++ src/cpp/espresso.hpp
@@ -0,0 +1,13 @@
+#ifndef ESPRESSO_HPP
+#define ESPRESSO_HPP
+
+#include "pmi/pmi.hpp"
+
+#include "logging.hpp"
+#include "mpi.hpp"
+#include "hello.hpp"
+
+/** initialize the Espresso extensions to Python. */
+void initPythonEspresso();
+
+#endif
