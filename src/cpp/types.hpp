47b1ab4915d35973c7d0e2b0307cd78409f10676
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2008-12-01 10:20:27 +0000

    Added include file types.hpp to distinguish between single/double precision.

---
+++ src/cpp/types.hpp
@@ -0,0 +1,10 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+// general type definition to choose between single and double precsion 
+
+// typedef float real;  // single precision
+
+typedef double real;  // double precision
+
+#endif 
