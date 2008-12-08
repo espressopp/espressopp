fe3020562d88fedc5c841902a61e6830850ddc38
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2008-12-08 13:40:04 +0000

    C++ified comm.cpp.

--- test/external/mpi/comm.cpp
+++ test/external/mpi/comm.cpp
@@ -3,22 +3,23 @@
 
 int main(int argc, char* argv[])
 {
-  MPI_Init(&argc, &argv);
+  MPI::Init(argc, argv);
 
-  int rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0) {
-    int value = 17;
-    int result = MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    if (result == MPI_SUCCESS)
+  // Execute the communication test only if there are more than one
+  // tasks
+  if (MPI::COMM_WORLD.Get_size() > 1) {
+    int rank = MPI::COMM_WORLD.Get_rank();
+    if (rank == 0) {
+      int value = 17;
+      MPI::COMM_WORLD.Send(&value, 1, MPI_INT, 1, 0);
       std::cout << "Rank 0 OK!" << std::endl;
-  } else if (rank == 1) {
-    int value;
-    int result = MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-			  MPI_STATUS_IGNORE);
-    if (result == MPI_SUCCESS && value == 17)
-      std::cout << "Rank 1 OK!" << std::endl;
+    } else if (rank == 1) {
+      int value;
+      MPI::COMM_WORLD.Recv(&value, 1, MPI_INT, 0, 0);
+      if (value == 17) std::cout << "Rank 1 OK!" << std::endl;
+    } 
   }
-  MPI_Finalize();
+  
+  MPI::Finalize();
   return 0;
 } 
