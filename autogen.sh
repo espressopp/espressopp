4eb13749b6b7b5c21bb61c25049e3282fd7246f4
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2009-01-14 10:23:18 +0000

    Some details fixes on the build system

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,10 @@
 
 rm -f config.cache src/cpp/acconfig.hpp
 
-aclocal -I build-aux/macros		&& \
-autoheader				&& \
-automake --copy --foreign --add-missing	&& \
-autoconf				&& \
+aclocal -I build-aux/macros	&& \
+autoheader			&& \
+automake --copy --add-missing	&& \
+autoconf			&& \
 exit 0
 
 exit 1
