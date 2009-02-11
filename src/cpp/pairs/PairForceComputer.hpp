64f0ac3c3ee8adee740152932b8eb2d1cbc3fe29
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2009-02-11 15:55:33 +0000

    Update, so that the doxygen docs looks better.

--- src/cpp/pairs/PairForceComputer.hpp
+++ src/cpp/pairs/PairForceComputer.hpp
@@ -1,3 +1,6 @@
+#ifndef _PAIRS_PAIRFORCECOMPUTER_HPP
+#define _PAIRS_PAIRFORCECOMPUTER_HPP
+
 #include "types.hpp"
 
 #include "interaction/Interaction.hpp"
@@ -15,39 +18,30 @@ namespace espresso {
      */
 
      class PairForceComputer: public ParticlePairComputer {
-
      private: 
-
        typedef particlestorage::ParticleStorage::PropertyTraits<Real3D>::Reference RealArrayRef;
-
        typedef espresso::interaction::Interaction Interaction;
 
        RealArrayRef force;
-
        const Interaction& interaction;
-    
        Real3D pressure;
-
        bool computesPressure;
        
      public:
-
       /** Constructor for force computations needs the force property and the interaction.
 
          \item _force is the particle property that stands for the force
          \itme _interaction provides the function that computes the force between two particles.
       */
-
        PairForceComputer(RealArrayRef _force, const Interaction& _interaction) 
-
          : force(_force),
            pressure(0.0),
            interaction(_interaction) {}
-    
+       
+       // TODO: why do I have to care what kind of reference to a particle I have?
        virtual void operator()(const Real3D &dist,
                                const espresso::particleset::ParticleSet::reference p1,
                                const espresso::particleset::ParticleSet::reference p2)
-
        {
          Real3D f = interaction.computeForce(dist, p1, p2);
 
@@ -59,3 +53,5 @@ namespace espresso {
      };
   }
 }
+
+#define
--- src/cpp/pairs/ParticlePairComputer.hpp
+++ src/cpp/pairs/ParticlePairComputer.hpp
@@ -1,54 +1,49 @@
-#ifndef _PAIRS_PARTICLEPAIRCOMPUTER
-#define _PAIRS_PARTICLEPAIRCOMPUTER
+#ifndef _PAIRS_PARTICLEPAIRCOMPUTER_HPP
+#define _PAIRS_PARTICLEPAIRCOMPUTER_HPP
 
 #include "types.hpp"
 
 #include "particleset/ParticleSet.hpp"
 
 namespace espresso {
+  
+  namespace pairs {
+    /** Abstract class that defines the operator() applied to particle pairs
+     */
+    template<class ParticleReference>
+    class ParticlePairComputerBase {
+      
+    public:
+      /// @name extended function object interface
+      //@{
+      typedef Real3D             first_argument_type;
+      typedef ParticleReference second_argument_type;
+      typedef ParticleReference  third_argument_type;
+      typedef void                       result_type;
+      //@}
 
-    namespace pairs {
+      /** Interface of the routine that is applied to particle pairs
+	  \param dist: distance vector between the two particles
+	  \param p1, p2: references to the two particles
 
-        /** Abstract class that defines the operator() applied to particle pairs
-	 */
+	  Note: The references are necessary if more property data of the particles is
+	  needed than only the distance.
+      */
+      virtual void operator()(const Real3D &dist, 
+			      const ParticleReference p1, 
+			      const ParticleReference p2) = 0;
+    };
 
-	template<class ParticleReference>
-        class ParticlePairComputerBase {
-
-        public:
-
-	    /// @name extended function object interface
-	    //@{
-	    typedef Real3D             first_argument_type;
-	    typedef ParticleReference second_argument_type;
-	    typedef ParticleReference  third_argument_type;
-	    typedef void                       result_type;
-	    //@}
-
-	    /** Interface of the routine that is applied to particle pairs
-
-		\param dist: distance vector between the two particles
-		\param p1, p2: references to the two particles
-
-		Note: The references are necessary if more property data of the particles is
-                needed than only the distance.
-	    */
-
-	    virtual void operator()(const Real3D &dist, 
-				    const ParticleReference p1, 
-				    const ParticleReference p2) = 0;
-        };
-
-        /** Abstract class that defines a function on pairs of particles */
-
-	typedef ParticlePairComputerBase<espresso::particlestorage::ParticleStorage::reference>
-                ParticlePairComputer;
-
-        /** Abstract class that defines a function on pairs of read-only particles */
-
-	typedef ParticlePairComputerBase<espresso::particlestorage::ParticleStorage::const_reference> 
-                ConstParticlePairComputer;
-    }
+    /** Abstract class that defines a function on pairs of particles */
+    class ParticlePairComputer: 
+      public ParticlePairComputerBase<espresso::particlestorage::ParticleStorage::reference> 
+    { };
+    
+    /** Abstract class that defines a function on pairs of read-only particles */
+    class ConstParticlePairComputer:
+      public ParticlePairComputerBase<espresso::particlestorage::ParticleStorage::const_reference> 
+    {};
+  }
 }
 
 #endif
