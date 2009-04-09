3d60d02937e1341e7b2577d863c6513a858d505a
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2009-04-09 22:55:20 +0000

    Updated the demo script, now demonstrates that particles can also be modified, by tagging them prior to write. Also reduces output :-).

--- examples/testParticles.py
+++ examples/testParticles.py
@@ -13,15 +13,32 @@ from _espresso import particles_Storage
 from _espresso import particles_PythonComputer
 from _espresso import Real3D
 from _espresso import Real3DProperty
+from _espresso import IntegerProperty
 import random
 
-class ParticleWriter(particles_PythonComputer):
-    def __init__(self, _property, storage):
-        self.property = _property
+# tag particles in a small sphere around (0,0,0)
+class ParticleTester(particles_PythonComputer):
+    def __init__(self, _position, _tag, storage):
         particles_PythonComputer.__init__(self, storage)
+        self.position = _position
+        self.tag = _tag
 
     def each(self, pid):
-        print("%d %s" % (pid, self.property[pid]))
+        pos = self.position[pid]
+        if pos*pos < 0.5:
+            self.tag[pid] = 1
+        else:
+            self.tag[pid] = 0
+
+class ParticleWriter(particles_PythonComputer):
+    def __init__(self, _property, _tag, storage):
+        particles_PythonComputer.__init__(self, storage)
+        self.property = _property
+        self.tag = _tag
+
+    def each(self, pid):
+        if self.tag[pid]:
+            print("%d %s" % (pid, self.property[pid]))
 
 storage = particles_Storage()
 
@@ -31,5 +48,12 @@ for count in range(0,100):
     pid = storage.addParticle()
     pos[pid] = Real3D(random.random(), random.random(), random.random())
 
-storage.foreach(ParticleWriter(pos, storage))
+# add property a posteriori
+tag = IntegerProperty(storage)
+
+#tag particles
+storage.foreach(ParticleTester(pos, tag, storage))
+
+# and print tagged ones
+storage.foreach(ParticleWriter(pos, tag, storage))
 
