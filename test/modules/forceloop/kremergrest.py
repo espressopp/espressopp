6ce62538088ef189c7e6c4783fd6ab4f677a207d
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2009-05-08 14:01:17 +0000

    Update Kremer-Grest Example to write PSF + XYZ file for visualization with VMD.

--- test/modules/forceloop/kremergrest.py
+++ test/modules/forceloop/kremergrest.py
@@ -20,6 +20,7 @@ from _espresso import Real3DProperty
 from _espresso import Real3D
 from _espresso import bc_PBC as PBC
 from _espresso import particles_PythonComputer 
+from _espresso import pairs_PythonComputer 
 from _espresso import particles_All
 from _espresso import pairs_All as All2
 from _espresso import pairs_List as List
@@ -32,7 +33,7 @@ from _espresso import integrator_VelocityVerlet as VelocityVerlet
 ##  Global Parameters of the simulation
 
 SIZE    = 16.0  # size of the simulation box
-NCHAINS = 5     # number of polymers
+NCHAINS = 20     # number of polymers
 NBEADS  = 5     # number of particles in one polymer
 
 class ParticleWriter(particles_PythonComputer):
@@ -43,6 +44,112 @@ class ParticleWriter(particles_PythonComputer):
 
     def each(self, pid):
         print("%d %s" % (pid, self.property[pid]))
+
+########################################################################
+#
+#  Class that allows to write XYZ coordinate files
+#
+########################################################################
+
+class XYZWriter(particles_PythonComputer):
+
+    """This class writes a XYZ coordinate file for all particles"""
+
+    def __init__(self, storage, size, position, filename):
+
+        particles_PythonComputer.__init__(self, storage)
+
+        self.outfile = open(filename, "w")
+        self.position = position
+        self.size     = size
+
+    def start(self):
+
+        self.outfile.write("%d\nAtoms\n" % (self.size,))
+
+    def each(self, pid):
+
+        pos = self.position[pid]
+        self.outfile.write("1 %f %f %f\n" % (pos[0], pos[1], pos[2]))
+
+    def close(self):
+
+        self.outfile.close()
+
+########################################################################
+#
+#  Class that write PSF file                         
+#
+########################################################################
+
+def writePSF(particleStorage, size, bondList, numbonds, filename):
+
+   # Helper class to write particles
+   
+   class ParticleWriter(particles_PythonComputer):
+
+      def __init__(self, storage, outfile):
+
+          particles_PythonComputer.__init__(self, storage)
+          self.outfile = outfile
+
+      def each(self, pid):
+
+          self.outfile.write("%8d" % (pid,))
+          self.outfile.write("      1")
+          self.outfile.write("%11d" % (pid,))
+          self.outfile.write("   ")
+          type   = 1
+          self.outfile.write("%7ld" % (type,))
+          self.outfile.write("0.000000       12.0000           0")
+          self.outfile.write("\n")
+
+   # Helper class to write particle pairs (here bonds)
+
+   class BondWriter(pairs_PythonComputer):
+
+      def __init__(self, pairs, outfile):
+
+         pairs_PythonComputer.__init__(self, pairs)
+         self.outfile = outfile
+         # counter for bonds needed to add CR
+         self.nbonds  = 0       
+
+      def each(self, pid1, pid2):
+
+          self.outfile.write("%8d%8d" % (pid1, pid2))
+          self.nbonds = self.nbonds + 1
+          if self.nbonds == 4:
+             self.outfile.write("\n")
+             self.nbonds = 0
+
+   #  Open PSF file and write header
+
+   outfile = open(filename, "w")
+
+   outfile.write("PSF\n")
+   outfile.write("\n")
+   outfile.write("       1 !NTITLE\n")
+   outfile.write(" REMARKS ESPResSo++ generated structure x-plor psf file\n")
+   outfile.write("\n")
+   outfile.write("%8d !NATOM\n" % (size,))
+
+   #  Write the info about the particles
+
+   writeParticles = ParticleWriter(particleStorage, outfile)
+   particleStorage.foreach(writeParticles)
+
+   # Write the header for the bonds
+
+   outfile.write("\n")
+   outfile.write("%8d !NBONDS\n" % (numbonds,))
+
+   # write the bonds into the file via PairComputer
+
+   writeBonds = BondWriter(bondList, outfile)
+   bondList.foreach(writeBonds)
+
+   outfile.close()
 
 import random
 
@@ -78,7 +185,7 @@ for chainid in range(NCHAINS):
     velocity[pid1] = Real3D(0.0)
     force[pid1]    = Real3D(0.0)
 
-    for beadid in range(NBEADS):
+    for beadid in range(NBEADS-1):
 
         # create a new position by random walk
         pos2 = pos1 + randomWalk(step=1.0)
@@ -109,7 +216,6 @@ allSet.foreach(ParticleWriter(position, particleStorage))
 allPairs = All2(pbc, allSet, position)
 
 ljint = LennardJones()
-ljint.set(1.0, 1.0, 2.5)
 
 fene = FENE()
 fene.set(1.0, 0.5, 0.1)
@@ -124,7 +230,26 @@ integrator.setTimeStep(0.005)
 integrator.addForce(ljint, allPairs)
 integrator.addForce(fene, bondList)
 
-integrator.run(100)
+writePSF(particleStorage, NCHAINS*NBEADS, 
+         bondList, NCHAINS*(NBEADS-1), "dump.psf")
 
-allSet.foreach(ParticleWriter(force, particleStorage))
+xyz = XYZWriter(particleStorage, NCHAINS*NBEADS, position, "dump.xyz")
+xyz.start()
+allSet.foreach(xyz)
+
+# equilibration
+
+N = 100
+for k in range(N):
+   sigma = (1.0 * (k+1)) / N
+   ljint.set(1.0, sigma, 2.5)
+   integrator.run(20)
+   xyz.start()
+   allSet.foreach(xyz)
+
+xyz.close()
+
+print 'Ready, call now: vmd dump.psf dump.xyz'
+
+# allSet.foreach(ParticleWriter(force, particleStorage))
 
