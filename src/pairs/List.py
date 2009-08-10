8cf98d0d3897ce215ce407d20358d01fac7001f5
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2009-08-10 09:57:42 +0000

    Small fix.

--- src/pairs/List.py
+++ src/pairs/List.py
@@ -5,12 +5,16 @@ from espresso.pairs.Set import *
 
 from _espresso import pairs_List
 class ListLocal(SetLocal, pairs_List):
-    def __init__(self, bc, storage, posProperty):
-        cxxinit(self, pairs_List, bc, storage, posProperty)
-
-#    def __init__(self, bc, storage1, storage2, posProperty1, posProperty2):
-#        cxxinit(self, pairs_List, bc, storage1, storage2, posProperty1, posProperty2)
+    def __init__(self, *args, **kwds):
+        cxxinit(self, pairs_List, *args, **kwds)
 
 if pmi.IS_CONTROLLER:
     class List(Set):
-        pmiproxydefs = dict(cls = 'espresso.pairs.ListLocal', pmicall = [ 'addPair', 'deletePair', 'size', 'findPair' ])
+        pmiproxydefs = \
+            dict(cls = 'espresso.pairs.ListLocal', 
+                 pmicall = [ 'addPair', 'deletePair', 'size', 'findPair' ])
+
+        # TODO: addPair should check whether the ids are valid
+
+    
+    
