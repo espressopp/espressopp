b4abd190db1388c9a0c5a845bfe85046f44dd97d
Axel Arnold <arnolda@icp.uni-stuttgart.de>
2009-08-25 21:01:10 +0000

    updated blockvector performance check.

--- test/modules/blockvector/blockvector.cpp
+++ test/modules/blockvector/blockvector.cpp
@@ -47,6 +47,45 @@ void bm_fill(BVClass &vec)
   }
 }
 
+void bm_shuffle(std::vector<VectorClass> &vec) NOINLINE;
+void bm_shuffle(std::vector<VectorClass> &vec)
+{
+  srand48(42);
+  for (size_t c = 0; c < 10000000; ++c) {
+    size_t b1 = lrand48() % nblocks;
+    if (vec[b1].empty()) continue;
+    size_t p1 = lrand48() % vec[b1].size();
+    size_t b2 = lrand48() % nblocks;
+    int val = vec[b1][p1];
+    vec[b2].push_back(val);
+    vec[b1].erase(vec[b1].begin() + p1);      
+  }
+}
+
+void bm_shuffle(BVClass &vec) NOINLINE;
+void bm_shuffle(BVClass &vec) {
+  srand48(42);
+  for (size_t c = 0; c < 10000000; ++c) {
+    size_t b1 = lrand48() % nblocks;
+    if (vec[b1].empty()) continue;
+    size_t p1 = lrand48() % vec[b1].size();
+    size_t b2 = lrand48() % nblocks;
+    int val = vec[b1][p1];
+    vec[b2].push_back(val);
+    vec[b1].erase(vec[b1].begin() + p1);      
+  }
+}
+
+BVClass::Block bm_test_pushback(BVClass &vec) NOINLINE;
+BVClass::Block bm_test_pushback(BVClass &vec) {
+  return vec[2345];
+}
+
+VectorClass::iterator bm_test_pushback(std::vector<VectorClass> &vec) NOINLINE;
+VectorClass::iterator bm_test_pushback(std::vector<VectorClass> &vec) {
+  return vec[2345].begin();
+}
+
 int main()
 {
   vector<VectorClass> stdvec;
@@ -65,6 +104,7 @@ int main()
     BOOST_FOREACH(BVClass::Block v, blkvec) { s += v.size(); c += v.capacity(); }
     cout << "filling BlockVector with " << s << " elements took " << time << " seconds" << endl;
     cout << "capacity is " << c << " elements" << endl;
+    cout << "gap size " << blkvec.target_gap_size() << endl;
   }
 
   /********************************************
@@ -84,17 +124,8 @@ int main()
    * random shuffling between neighbor blocks - vector
    ********************************************/
   {
-    srand48(42);
     timer.reset();
-    for (size_t c = 0; c < 10000000; ++c) {
-      size_t b1 = lrand48() % nblocks;
-      if (stdvec[b1].empty()) continue;
-      size_t p1 = lrand48() % stdvec[b1].size();
-      size_t b2 = lrand48() % nblocks;
-      int val = stdvec[b1][p1];
-      stdvec[b2].push_back(val);
-      stdvec[b1].erase(stdvec[b1].begin() + p1);      
-    }
+    bm_shuffle(stdvec);
     float time = timer.getElapsedTime();
     size_t s = 0, c = 0;
     BOOST_FOREACH(VectorClass &v, stdvec) { s += v.size(); c += v.capacity(); }
@@ -106,22 +137,14 @@ int main()
    * random shuffling between neighbor blocks - BlockVector
    ********************************************/
   {
-    srand48(42);
     timer.reset();
-    for (size_t c = 0; c < 10000000; ++c) {
-      size_t b1 = lrand48() % nblocks;
-      if (blkvec[b1].empty()) continue;
-      size_t p1 = lrand48() % blkvec[b1].size();
-      size_t b2 = lrand48() % nblocks;
-      int val = blkvec[b1][p1];
-      blkvec[b2].push_back(val);
-      blkvec[b1].erase(blkvec[b1].begin() + p1);      
-    }
+    bm_shuffle(blkvec);
     float time = timer.getElapsedTime();
     size_t s = 0, c = 0;
     BOOST_FOREACH(BVClass::Block v, blkvec) { s += v.size(); c += v.capacity(); }
     cout << "shuffling BlockVector took " << time << " seconds" << endl;    
     cout << "final usage is still " << s << " out of a capacity of " << c << " elements" << endl;
+    cout << "gap size " << blkvec.target_gap_size() << endl;
   }
 
   return 0;
