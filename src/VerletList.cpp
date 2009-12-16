b101ae553c942591a02cdb2958b3f3a63811a5b1
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2009-12-16 13:34:53 +0000

    VerletList first version + Unit test

---
+++ src/VerletList.cpp
@@ -0,0 +1,81 @@
+#include "Cell.hpp"
+
+#include "System.hpp"
+#include "VerletList.hpp"
+
+using namespace espresso;
+
+LOG4ESPP_LOGGER(VerletList::theLogger, "VerletList");
+
+VerletList::VerletList(System::SelfPtr system, double cut)
+
+{
+  cutsq = cut * cut;
+
+  std::vector<Cell>& localCells = system->storage->getLocalCells();
+ 
+  for (size_t c = 0; c < localCells.size(); c++) {
+
+    Cell* localCell = &localCells[c];
+
+    LOG4ESPP_INFO(theLogger, "build verlet list for local cell " << c);
+
+    // Loop cell neighbors
+
+    std::vector<Cell*>& neighborCells = localCell->neighborCells;
+ 
+    for (size_t n = 0; n < neighborCells.size(); n++) {
+
+      LOG4ESPP_DEBUG(theLogger, "loop cell pair " << c << " x " << n);
+
+      Cell* neighborCell = neighborCells[n];
+
+      // avoid double cells
+
+      if (localCell - neighborCell < 0) continue;
+
+      // now build all parir of localCell / neighborCell
+ 
+      for (size_t p1 = 0; p1 < localCell->particles.size(); p1++) {
+
+        Particle* pt1  = &localCell->particles[p1];
+        double*   pos1 = pt1->r.p;
+
+        size_t nparticles = neighborCell->particles.size();
+
+        // Avoid double entries (i,j) and (j,i) in local cell
+
+        if (localCell == neighborCell) {
+
+          // avoid double particle pairs in a cell 
+
+          nparticles = p1 - 1;
+
+        }
+
+        for (size_t p2 = 0; p2 < nparticles; p2++) {
+
+          Particle* pt2  = &neighborCell->particles[p1];
+          double*   pos2 = pt2->r.p;
+
+          double d[3];
+
+          d[0] = pos1[0] - pos2[0];
+          d[1] = pos1[1] - pos2[1];
+          d[2] = pos1[2] - pos2[2];
+
+          double distsq = d[0]*d[0] + d[1]*d[1] + d[2]*d[2];
+
+          if (distsq >= cutsq) continue;
+
+          myList.push_back(std::pair<Particle*, Particle*>(pt1, pt2));
+        }
+      } 
+    }
+  }
+}
+
+VerletList::~VerletList()
+{
+}
+
---
+++ src/storage/unittest/PTestVerletList.cpp
@@ -0,0 +1,71 @@
+#define PARALLEL_TEST_MODULE DomainDecomposition
+#include "ut.hpp"
+#include <memory>
+
+#include "mpi.hpp"
+#include "logging.hpp"
+#include "esutil/RNG.hpp"
+#include "../DomainDecomposition.hpp"
+#include "../VerletList.hpp"
+#include "System.hpp"
+
+using namespace espresso;
+
+struct LoggingFixture {  
+  LoggingFixture() { 
+    LOG4ESPP_CONFIGURE();
+    log4espp::Logger::getRoot().setLevel(log4espp::Logger::TRACE);
+  }
+};
+
+BOOST_GLOBAL_FIXTURE(LoggingFixture);
+
+struct Fixture {
+  DomainDecomposition::SelfPtr domdec;
+  System::SelfPtr system;
+
+  Fixture() {
+    real boxL[3] = { 2.0, 2.0, 3.0 };
+    int nodeGrid[3] = { mpiWorld.size(), 1, 1 };
+    int cellGrid[3] = { 2, 2, 3 };
+    system = make_shared< System >();
+    system->setBoxL(boxL);
+    domdec = make_shared< DomainDecomposition >(system,
+    						mpiWorld,
+    						nodeGrid,
+    						cellGrid,
+    						true);
+    int initPPN = 4;
+    esutil::RNG rng;
+    boost::mpi::communicator comm;
+
+    for (int i = 0; i < initPPN; ++i) {
+      real pos[3] = { 2*rng(), 2*rng(), 3*rng() };
+      domdec->addParticle(i, pos);
+    }
+
+    system->storage = domdec;
+
+  }
+};
+
+BOOST_GLOBAL_FIXTURE(Fixture);
+
+BOOST_FIXTURE_TEST_CASE(buildVerletList, Fixture) 
+{
+  BOOST_MESSAGE("starting to build verlet lists");
+
+  double cut = 2.5;
+
+  VerletList::SelfPtr vl = make_shared<VerletList>(system, cut);
+
+  VerletList::pairList pairs = vl->getPairs();
+
+  printf("# of verlet list pairs = %d\n", pairs.size());
+
+  for (size_t i = 0; i < pairs.size(); i++) {
+    printf("pair %d = %d %d\n", i, 1, 2);
+  }
+
+  // BOOST_CHECK_EQUAL(total, comm.size()*initPPN);
+}
