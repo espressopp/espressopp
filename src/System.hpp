65fc34ee0013bc3fa1003dcf4a992f809b323f34
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2010-01-26 16:43:10 +0000

    System with first interaction list + RNG.

--- src/System.hpp
+++ src/System.hpp
@@ -2,12 +2,24 @@
 #define _SYSTEM_HPP
 
 #include "types.hpp"
+#include "esutil/RNG.hpp"
 
 namespace espresso {
+
+  namespace interaction { class Interaction; }
+
+  typedef std::vector< shared_ptr<interaction::Interaction> > InteractionList;
+
   class System {
+
   public:
     shared_ptr< class Storage > storage;
     shared_ptr< class BC > bc;
+
+    InteractionList shortRangeInteractions;
+
+    espresso::esutil::RNG rng; //<! common random number generator
+
   };
 }
 #endif
