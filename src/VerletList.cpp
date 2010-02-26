9b28e69468d26368c732ca0d820f4ddf9408fbad
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2010-02-26 14:16:06 +0000

    VerletLists are setup with CellListAllPairsIterator, unittest successful.

--- src/VerletList.cpp
+++ src/VerletList.cpp
@@ -8,8 +8,10 @@
 #include "System.hpp"
 #include "Storage.hpp"
 #include "BC.hpp"
+#include "iterator/CellListAllPairsIterator.hpp"
 
 using namespace espresso;
+using namespace espresso::iterator;
 
 LOG4ESPP_LOGGER(VerletList::theLogger, "VerletList");
 
@@ -21,74 +23,13 @@ VerletList::VerletList(shared_ptr< System > system, real cut)
 
   cutsq = cut * cut;
 
-  std::vector< Cell* >& localCells = system->storage->getRealCells();
- 
-  bc = system->bc;
+  CellList cl = system->storage->getRealCells();
 
-  for (size_t c = 0; c < localCells.size(); c++) {
+  for (CellListAllPairsIterator it(cl);it.isValid(); ++it) {
 
-    Cell* localCell = localCells[c];
-
-    // Check for pairs in the cell itself
-
-    size_t nparticles = localCell->particles.size();
-
-    LOG4ESPP_DEBUG(theLogger, "find pairs of real cell " << c
-		   << " with local index " << localCell - system->storage->getFirstCell()
-		   << " with " << nparticles << " particles");
-
-    int c = myList.size();
-
-    for (size_t p1 = 0; p1 < nparticles; p1++) {
-
-      Particle& pt1  = localCell->particles[p1];
-
-      for (size_t p2 = p1 + 1; p2 < nparticles; p2++) {
-
-        checkPair(pt1, localCell->particles[p2]);
-      }
-      
-    }
-
-    LOG4ESPP_INFO(theLogger, "self size " << myList.size() - c);
-
-    // Loop cell neighbors
-
-    std::vector<NeighborCellInfo>& neighborCells = localCell->neighborCells;
- 
-    LOG4ESPP_DEBUG(theLogger, "find pairs of real cell " << c
-		   << " with local index " << localCell - system->storage->getFirstCell()
-		   << " with " << neighborCells.size() << " neighbored cells");
-
-    for (size_t n = 0; n < neighborCells.size(); n++) {
-
-      Cell* neighborCell = neighborCells[n].cell;
-
-      int nPNeighbor = neighborCell->particles.size();
-
-      LOG4ESPP_DEBUG(theLogger, "Neighbor " << n
-		     << " with local index " << neighborCell - system->storage->getFirstCell()
-		     << " has " << neighborCell->particles.size() << " particles");
-
-      if (!neighborCells[n].useForAllPairs || nPNeighbor == 0) continue;
-
-      // now build all parir of localCell / neighborCell
-
-      int c = myList.size();
- 
-      for (size_t p1 = 0; p1 < nparticles; p1++) {
-
-        Particle& pt1  = localCell->particles[p1];
-
-        for (size_t p2 = 0; p2 < nPNeighbor; p2++) {
-
-          checkPair(pt1, neighborCell->particles[p2]);
-        }
-      }
-
-      LOG4ESPP_DEBUG(theLogger, "cross size " << myList.size() - c);
-    }
+    checkPair(*it->first, *it->second);
   }
+
 }
 
 /*-------------------------------------------------------------*/
