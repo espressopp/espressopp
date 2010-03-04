2adce7dfa741e0b920651414b718c960c0dfbfb8
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2010-03-04 16:42:45 +0000

    Merged VelocityVerlet unit tests into one correctly named file.

--- src/integrator/unittest/PTestVelocityVerlet.cpp
+++ src/integrator/unittest/PTestVelocityVerlet.cpp
@@ -1,24 +1,25 @@
-#define PARALLEL_TEST_MODULE VelocityVerletIntegrator
+#define PARALLEL_TEST_MODULE VelocityVerlet
 #include "ut.hpp"
-#include <memory>
+//#include <memory>
 
+#include "types.hpp"
 #include "mpi.hpp"
 #include "logging.hpp"
 #include "esutil/RNG.hpp"
 #include "storage/DomainDecomposition.hpp"
-#include "integrator/VelocityVerlet.hpp"
 #include "interaction/LennardJones.hpp"
-#include "bc/OrthorhombicBC.hpp"
+#include "integrator/VelocityVerlet.hpp"
 #include "iterator/CellListIterator.hpp"
+#include "bc/OrthorhombicBC.hpp"
 #include "System.hpp"
 #include "VerletList.hpp"
 #include "Real3D.hpp"
 
 using namespace espresso;
 using namespace storage;
-using namespace iterator;
-using namespace integrator;
 using namespace interaction;
+using namespace integrator;
+using namespace iterator;
 using namespace bc;
 using namespace esutil;
 
@@ -26,17 +27,130 @@ struct LoggingFixture {
   LoggingFixture() { 
     LOG4ESPP_CONFIGURE();
     log4espp::Logger::getRoot().setLevel(log4espp::Logger::WARN);
-    log4espp::Logger::getInstance("VerletList").setLevel(log4espp::Logger::WARN);
+    // log4espp::Logger::getInstance("VerletList").setLevel(log4espp::Logger::WARN);
+    // log4espp::Logger::getInstance("Storage").setLevel(log4espp::Logger::DEBUG);
     log4espp::Logger::getInstance("MDIntegrator").setLevel(log4espp::Logger::WARN);
   }
 };
 
-static real cutoff = 1.0;
-
+static real cutoff1 = 1.4;
+static real cutoff2 = 1.0;
+static int  niter  = 60;
+static double timestep = 0.005;
 static int N = 3;
 static real size = N;
 
 BOOST_GLOBAL_FIXTURE(LoggingFixture);
+
+struct Fixture {
+  shared_ptr<DomainDecomposition> domdec;
+  shared_ptr<System> system;
+
+  Fixture() {
+
+    int N = 3;  // number of particles in each dimension
+
+    real density = 1.0;
+
+    real SIZE = N * density;
+
+    BOOST_MESSAGE("box SIZE = " << SIZE << ", density = " << density);
+
+    Real3D boxL(SIZE, SIZE, SIZE);
+
+    real skin   = 0.3;
+
+    ConstReal3DRef boxLRef(boxL);
+
+    int nodeGrid[3] = { 1, 1, mpiWorld.size() };
+    int cellGrid[3] = { 1, 1, 1 };
+
+    for (int i = 0; i < 3; i++) {
+      int ncells = 1;
+       while (SIZE / (ncells * nodeGrid[i]) >= cutoff1) {
+         ncells ++;
+       }
+       ncells--;
+       cellGrid[i] = ncells;
+    }
+
+    BOOST_MESSAGE("ncells in each dim / proc: " << cellGrid[0] << " x " <<
+                                 cellGrid[1] << " x " << cellGrid[2]);
+
+    system = make_shared< System >();
+    system->bc = make_shared< OrthorhombicBC >(system, boxLRef);
+    system->skin = skin;
+    domdec = make_shared< DomainDecomposition >(system,
+                                                mpiWorld,
+                                                nodeGrid,
+                                                cellGrid);
+    esutil::RNG rng;
+
+    int id = 0;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < N; k++) {
+
+          int m = (i + 2*j + 3*k) % 11;
+          real r = 0.45 + m * 0.01;
+          real x = (i + r) / N * SIZE;
+          real y = (j + r) / N * SIZE; 
+          real z = (k + r) / N * SIZE;
+          real pos[3] = { x, y, z };
+      
+          if (mpiWorld.rank() == 0) {
+          
+            printf("add particle at %f %f %f\n", x, y, z);
+            domdec->addParticle(id, pos);
+
+          }
+
+          id++;
+        }
+      }
+    }
+
+    system->storage = domdec;
+
+    BOOST_MESSAGE("number of particles in storage =  " << 
+                  domdec->getNRealParticles());
+  }
+};
+
+// BOOST_GLOBAL_FIXTURE(Fixture);
+
+BOOST_FIXTURE_TEST_CASE(calcEnergy, Fixture)
+{
+  BOOST_MESSAGE("define LJ potential");
+
+  // define a potential
+
+  shared_ptr<LennardJones> lj = make_shared<LennardJones>();
+  lj->setParameters(0, 0, 1.0, 1.0, cutoff1);
+
+  lj->enableShift(0, 0);
+  system->shortRangeInteractions.push_back(lj);
+
+  BOOST_MESSAGE("define Velocity Verlet integrator");
+
+  shared_ptr<MDIntegrator> integrator = 
+     make_shared<VelocityVerlet>(system);
+
+  integrator->setTimeStep(timestep);
+
+  BOOST_MESSAGE("run " << niter << " iterations with timestep " << timestep);
+
+  integrator->run(niter);
+
+  BOOST_MESSAGE("ready");
+}
+
+
+
+
+
+
+
 
 // sets up a storage with particles in a lattice
 
@@ -68,7 +182,7 @@ struct DomainFixture {
 
        int ncells = 1;
 
-       while (SIZE / (ncells * nodeGrid[i]) >= cutoff) {
+       while (SIZE / (ncells * nodeGrid[i]) >= cutoff2) {
           ncells ++;
        }
 
@@ -123,7 +237,7 @@ struct LatticeFixture : DomainFixture {
 
     // loop over all particles of the real cells and set velocity in x - direction
 
-    for(CellListIterator cit(realCells); !cit.isDone(); ++cit) {
+    for (CellListIterator cit(realCells); !cit.isDone(); ++cit) {
       cit->m.v[0] = 1.0;
     }
 
@@ -141,7 +255,7 @@ BOOST_FIXTURE_TEST_CASE(moveParticles, LatticeFixture)
   // define a 0 potential, but with a cutoff
 
   shared_ptr<LennardJones> lj = make_shared<LennardJones>();
-  lj->setParameters(0, 0, 0.0, 0.0, cutoff);
+  lj->setParameters(0, 0, 0.0, 0.0, cutoff2);
 
   system->shortRangeInteractions.push_back(lj);
 
