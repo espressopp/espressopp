b30c21ba03eb7edef5ebbd4453ffff9d29075d45
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2010-03-04 15:44:12 +0000

    Parallel version of test Verlet List.

--- src/unittest/PTestVerletList.cpp
+++ src/unittest/PTestVerletList.cpp
@@ -18,6 +18,12 @@ using namespace bc;
 using namespace esutil;
 using namespace espresso::iterator;
 
+/***********************************************************************************
+*                                                                                  *
+*  Global settings for problem                                                     *
+*                                                                                  *
+***********************************************************************************/
+
 struct LoggingFixture {  
   LoggingFixture() { 
     LOG4ESPP_CONFIGURE();
@@ -27,34 +33,56 @@ struct LoggingFixture {
   }
 };
 
-static real cutoff = 1.001;
+static real cutoff = 1.733;  // largest cutoff
 
-static int  N = 4;  // particles in each dimension
+static int  N = 6;  // particles in each dimension
 
-static real density = 0.6;
+static real density = 1.0;
 
 BOOST_GLOBAL_FIXTURE(LoggingFixture);
 
 // sets up a storage with particles in a lattice
 
-struct LatticeFixture {
+struct DomainFixture {
 
   shared_ptr<DomainDecomposition> domdec;
   shared_ptr<System> system;
 
-  LatticeFixture() {
+  real SIZE;
 
-    real SIZE = N;
+  DomainFixture(double density) {
+
+    SIZE = pow(N * N * N / density, 1.0/3.0) ;
 
     BOOST_MESSAGE("box SIZE = " << SIZE << ", density = 1.0");
 
     Real3D boxL(SIZE, SIZE, SIZE);
 
     real skin   = 0.3;
-    int  ncells = SIZE / (2 * cutoff + skin) + 1;
+    
+    int nodeGrid[3] = { 1, 1, mpiWorld.size() };
 
-    int nodeGrid[3] = { 1, 1, 1 };
-    int cellGrid[3] = { ncells, ncells, ncells };
+    int cellGrid[3] = { 1, 1, 1 };
+
+    for (int i = 0; i < 3; i++) {
+
+       // take largest number of cells, but cell size must be >= cutoff
+       // so cutoff used here should be the largest cutoff
+
+       int ncells = 1;
+
+       while (SIZE / (ncells * nodeGrid[i]) >= cutoff) {
+          ncells ++;
+       }
+  
+       ncells--;
+
+       cellGrid[i] = ncells;
+    }
+
+    BOOST_MESSAGE("ncells in each dim / proc: " << cellGrid[0] << " x " <<
+                                 cellGrid[1] << " x " << cellGrid[2]);
+
     system = make_shared< System >();
     system->bc = make_shared< OrthorhombicBC >(system, boxL);
     system->skin = skin;
@@ -62,6 +90,16 @@ struct LatticeFixture {
                                                 mpiWorld,
                                                 nodeGrid,
                                                 cellGrid);
+    system->storage = domdec;
+  }
+};
+
+// sets up a storage with particles in a lattice
+
+struct LatticeFixture : DomainFixture {
+
+  LatticeFixture() : DomainFixture(1.0) {
+
     int id = 0;
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
@@ -74,15 +112,15 @@ struct LatticeFixture {
           real z = (k + r) / N * SIZE;
           real pos[3] = { x, y, z };
       
-          BOOST_MESSAGE("add particle at pos " << x << " " << y << " " << z);
+          if (mpiWorld.rank() == 0) {
+            BOOST_MESSAGE("add particle at pos " << x << " " << y << " " << z);
+            domdec->addParticle(id, pos);
+          }
 
-          domdec->addParticle(id, pos);
           id++;
         }
       }
     }
-
-    system->storage = domdec;
 
     BOOST_MESSAGE("number of lattice particles in storage = " <<
                    domdec->getNRealParticles());
@@ -91,34 +129,12 @@ struct LatticeFixture {
   }
 };
 
-// sets up a storage with random particles 
+// sets up a storage with random particles
 
-struct RandomFixture {
+struct RandomFixture : DomainFixture {
 
-  shared_ptr<DomainDecomposition> domdec;
-  shared_ptr<System> system;
+  RandomFixture() : DomainFixture(density) {
 
-  RandomFixture() {
-
-    real SIZE = pow(N * N * N / density, 1.0/3.0) ;
-
-    Real3D boxL(SIZE, SIZE, SIZE);
-
-    real skin   = 0.3;
-
-    int  ncells = SIZE / (2 * cutoff + skin) + 1;
-
-    printf("cellGrid = %d x %d x %d\n", ncells, ncells, ncells);
-
-    int nodeGrid[3] = { 1, 1, 1 };
-    int cellGrid[3] = { ncells, ncells, ncells };
-    system = make_shared< System >();
-    system->bc = make_shared< OrthorhombicBC >(system, boxL);
-    system->skin = skin;
-    domdec = make_shared< DomainDecomposition >(system,
-                                                mpiWorld,
-                                                nodeGrid,
-                                                cellGrid);
     esutil::RNG rng;
 
     int id = 0;
@@ -131,13 +147,14 @@ struct RandomFixture {
 
       real pos[3] = { x, y, z };
 
-      BOOST_MESSAGE("add particle at pos " << x << " " << y << " " << z);
-
-      domdec->addParticle(id, pos);
+      if (mpiWorld.rank() == 0) {
+        BOOST_MESSAGE("add particle at pos " << x << " " << y << " " << z);
+        domdec->addParticle(id, pos);
+      }
 
       id++;
     }
-      
+
     system->storage = domdec;
 
     BOOST_MESSAGE("number of random particles in storage = " <<
@@ -157,32 +174,42 @@ BOOST_FIXTURE_TEST_CASE(LatticeTest, LatticeFixture)
 
   // there are no pairs for cutoff 0.0
 
-  BOOST_CHECK_EQUAL(pairs.size(), 0);
+  int localPairs = pairs.size();
+  int globalPairs;
+
+  boost::mpi::all_reduce(mpiWorld, localPairs, globalPairs, std::plus<int>());
+
+  BOOST_CHECK_EQUAL(globalPairs, 0);
 
   vl = make_shared< VerletList >(system, 1.001);
 
   // there are N * N * N * 6 / 2 pairs in cutoff 1.0
 
   pairs = vl->getPairs();
+  localPairs = pairs.size();
+  boost::mpi::all_reduce(mpiWorld, localPairs, globalPairs, std::plus<int>());
 
-  BOOST_CHECK_EQUAL(pairs.size(), N * N * N * 3);
+  BOOST_CHECK_EQUAL(globalPairs, N * N * N * 3);
 
   // there are N * N * N * 18 / 2 pairs in cutoff  sqrt(2.0)
 
   vl = make_shared< VerletList >(system, pow(2.001, 0.5));
 
   pairs = vl->getPairs();
+  localPairs = pairs.size();
+  boost::mpi::all_reduce(mpiWorld, localPairs, globalPairs, std::plus<int>());
 
-  BOOST_CHECK_EQUAL(pairs.size(), N * N * N * 9);
+  BOOST_CHECK_EQUAL(globalPairs, N * N * N * 9);
 
   vl = make_shared< VerletList >(system, pow(3.001, 0.5));
 
   pairs = vl->getPairs();
+  localPairs = pairs.size();
+  boost::mpi::all_reduce(mpiWorld, localPairs, globalPairs, std::plus<int>());
 
   // there are N * N * N * 26 / 2 pairs in cutoff 
 
-  BOOST_CHECK_EQUAL(pairs.size(), N * N * N * 13);
-
+  BOOST_CHECK_EQUAL(globalPairs, N * N * N * 13);
 }
 
 real getDistSqr(Particle& p1, Particle& p2)
@@ -213,7 +240,11 @@ BOOST_FIXTURE_TEST_CASE(RandomTest, RandomFixture)
                  << ", dist = " << sqrt(getDistSqr(*p1, *p2)));
   }
 
-  // count pairs in a N square loop 
+  int totalPairs1;
+
+  boost::mpi::all_reduce(mpiWorld, (int) pairs.size(), totalPairs1, std::plus<int>());
+
+  // count pairs in a N square loop
 
   int count = 0;
 
@@ -230,7 +261,7 @@ BOOST_FIXTURE_TEST_CASE(RandomTest, RandomFixture)
 
       if (distsqr >= cutoff * cutoff) continue;
 
-      BOOST_MESSAGE("pair " << count << ": " 
+      BOOST_MESSAGE("pair " << count << ": "
                  << cit1->p.id << " " << cit2->p.id
                  << ", dist = " << sqrt(distsqr));
 
@@ -238,5 +269,12 @@ BOOST_FIXTURE_TEST_CASE(RandomTest, RandomFixture)
     }
   }
 
-  BOOST_CHECK_EQUAL(count, pairs.size());
+  int totalPairs2;
+
+  boost::mpi::all_reduce(mpiWorld, count, totalPairs2, std::plus<int>());
+
+  BOOST_CHECK_EQUAL(totalPairs1, totalPairs2);
+
+  // BOOST_CHECK_EQUAL(pairs.size(), count) on each processor is not always the case
 }
+
