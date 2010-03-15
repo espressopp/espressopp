a5fb3f2f6f1465e91c975a0ece2ae3f554dd505a
Olaf Lenz <olenz@icp.uni-stuttgart.de>
2010-03-15 10:26:51 +0000

    Added new tool espp_format that automatically reformats C++ source code.

--- src/interaction/Interaction.cpp
+++ src/interaction/Interaction.cpp
@@ -1,8 +1,6 @@
 #include "Interaction.hpp"
 
 namespace espresso {
-  namespace interaction {
-    LOG4ESPP_LOGGER(Interaction::theLogger, "Interaction");
-  }
-}
-
+	namespace interaction {
+		LOG4ESPP_LOGGER(Interaction::theLogger, "Interaction");
+}}
