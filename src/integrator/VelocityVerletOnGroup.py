ecdd9837a5c073307a1014fdd2004f45d045465b
Victor Ruehle <ruehle@mpip-mainz.mpg.de>
2010-11-30 17:28:12 +0100

    fixed typo

--- src/integrator/VelocityVerletOnGroup.py
+++ src/integrator/VelocityVerletOnGroup.py
@@ -4,7 +4,7 @@ from espresso import pmi
 from espresso.integrator.MDIntegrator import *
 from _espresso import integrator_VelocityVerletOnGroup
 
-class VelocityOnGroupVerletLocal(MDIntegratorLocal, integrator_VelocityVerletOnGroup):
+class VelocityVerletOnGroupLocal(MDIntegratorLocal, integrator_VelocityVerletOnGroup):
     'The (local) Velocity Verlet Integrator.'
     def __init__(self, system, group):
         cxxinit(self, integrator_VelocityVerletOnGroup, system, group)
