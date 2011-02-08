f344951d08bf6c7a8928690994bb72ba97f68951
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-02-08 18:02:50 +0100

    added a note about cmake to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+
+echo "############################################"
+echo "# We offer cmake as an alternative!        #"
+echo "# consider running 'cmake .; make'         #"
+echo "# instead of 'autogen.sh; configure; make' #"
+echo "############################################"
+sleep 1
+
 (cd src; python collect_classes.py)
-exec autoreconf -v -i -Wall
+autoreconf -v -i -Wall
+
+cp configure configure.tmp
+cat >configure <<EOF
+#!/bin/sh
+echo "############################################"
+echo "# We offer cmake as an alternative!        #"
+echo "# Please consider running 'cmake .; make'  #"
+echo "# instead of './configure; make'           #"
+echo "############################################"
+sleep 1
+EOF
+cat configure.tmp >> configure
+rm configure.tmp
