500faa9ce88f40be1463a1eb9f54adb7c0e53aea
Torsten Stuehn <stuehn@mpip-mainz.mpg.de>
2011-07-27 14:49:52 +0200

    some little error checking in decomp.py

--- src/tools/decomp.py
+++ src/tools/decomp.py
@@ -22,7 +22,9 @@ def nodeGrid(n):
 
 def cellGrid(box_size, node_grid, rc, skin):
   rc_skin = rc + skin
-  ix = max(1, box_size[0] / (rc_skin * node_grid[0]))
+  if rc_skin == 0:
+    print "ERROR: number of cells is infinity ( cutoff + skin is 0 )"
+  ix = max(1, box_size[0] / (rc_skin * node_grid[0])) 
   iy = max(1, box_size[1] / (rc_skin * node_grid[1]))
   iz = max(1, box_size[2] / (rc_skin * node_grid[2]))
   return Int3D(ix, iy, iz)
