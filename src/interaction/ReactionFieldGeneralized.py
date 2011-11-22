769ba0d67a9526ac555ae5d3ab54d96c5704ccf2
Stas Bevc <stas.bevc@cmm.ki.si>
2011-11-22 15:13:43 +0100

    Changes and fixes in gen. reaction field.

--- src/interaction/ReactionFieldGeneralized.py
+++ src/interaction/ReactionFieldGeneralized.py
@@ -11,14 +11,14 @@ from _espresso import interaction_ReactionFieldGeneralized, \
 
 class ReactionFieldGeneralizedLocal(PotentialLocal, interaction_ReactionFieldGeneralized):
     'The (local) ReactionFieldGeneralized potential.'
-    def __init__(self, kappa=0.0, epsilon1=1.0, epsilon2=80.0, cutoff=infinity, shift="auto"):
+    def __init__(self, qq=0.0, kappa=0.0, epsilon1=1.0, epsilon2=80.0, cutoff=infinity, shift="auto"):
         """Initialize the local ReactionFieldGeneralized object."""
         if shift =="auto":
             if not (pmi._PMIComm and pmi._PMIComm.isActive()) or pmi._MPIcomm.rank in pmi._PMIComm.getMPIcpugroup():
-                cxxinit(self, interaction_ReactionFieldGeneralized, kappa, epsilon1, epsilon2, cutoff)
+                cxxinit(self, interaction_ReactionFieldGeneralized, qq, kappa, epsilon1, epsilon2, cutoff)
         else:
             if not (pmi._PMIComm and pmi._PMIComm.isActive()) or pmi._MPIcomm.rank in pmi._PMIComm.getMPIcpugroup():
-                cxxinit(self, interaction_ReactionFieldGeneralized, kappa, epsilon1, epsilon2, cutoff, shift)
+                cxxinit(self, interaction_ReactionFieldGeneralized, qq, kappa, epsilon1, epsilon2, cutoff, shift)
 
 class VerletListReactionFieldGeneralizedLocal(InteractionLocal, interaction_VerletListReactionFieldGeneralized):
     'The (local) ReactionFieldGeneralized interaction using Verlet lists.'
