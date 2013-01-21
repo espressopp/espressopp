c6cb3864228cf60740c9c0373011068f280a9d14
Vitaliy Starchenko <starchen@mpip-mainz.mpg.de>
2013-01-21 15:17:00 +0100

    DPD thermostat can now be connected and disconnected

--- src/integrator/DPDThermostat.py
+++ src/integrator/DPDThermostat.py
@@ -1,9 +1,10 @@
 from espresso.esutil import cxxinit
 from espresso import pmi
 
+from espresso.integrator.Extension import *
 from _espresso import integrator_DPDThermostat 
 
-class DPDThermostatLocal(integrator_DPDThermostat):
+class DPDThermostatLocal(ExtensionLocal, integrator_DPDThermostat):
     'The (local) Velocity Verlet Integrator.'
     def __init__(self, system, vl):
         if not (pmi._PMIComm and pmi._PMIComm.isActive()) or pmi._MPIcomm.rank in pmi._PMIComm.getMPIcpugroup():
@@ -14,7 +15,7 @@ class DPDThermostatLocal(integrator_DPDThermostat):
     #        self.cxxclass.enableAdress(self);
 
 if pmi.isController :
-    class DPDThermostat(object):
+    class DPDThermostat(Extension):
         __metaclass__ = pmi.Proxy
         pmiproxydefs = dict(
             cls =  'espresso.integrator.DPDThermostatLocal',
