2aaadcb4591943c95fbb15bb42ff6eaeb281e64f
Vitaliy Starchenko <starchen@mpip-mainz.mpg.de>
2013-08-07 16:20:14 +0200

    description added to DumpXYZ

--- src/io/DumpXYZ.py
+++ src/io/DumpXYZ.py
@@ -4,20 +4,34 @@
 *******************************************
 
 * `dump()`
-  write configuration to trajectory XYZ file. Default filename is "out.xyz"
+  write configuration to trajectory XYZ file. By default filename is "out.xyz", 
+  coordinates are folded.
 
+  Properties
+
+* `filename`
+  Name of trajectory file. By default trajectory file name is "out.xyz"
+
+* `unfolded`
+  False if coordinates are folded, True if unfolded. By default - False
+  
 usage:
 
-storing trajectory
+writing down trajectory
 
-******************************** Should be modified below
+>>> dump_conf_xyz = espresso.io.DumpXYZ(system, integrator, filename='trajectory.xyz')
+>>> for i in range (200):
+>>>   integrator.run(10)
+>>>   xyz.dump()
 
->>> configurations = espresso.ConfigurationsExt(system)
->>> configurations.gather()
->>> for k in range(100):
->>>   integrator.run(100)
->>>   configurations.gather()
+writing down trajectory using ExtAnalyze extension
 
+>>> dump_conf_xyz = espresso.io.DumpXYZ(system, integrator, filename='trajectory.xyz')
+>>> ext_analyze = espresso.integrator.ExtAnalyze(dump_conf_xyz, 10)
+>>> integrator.addExtension(ext_analyze)
+>>> integrator.run(2000)
+
+both exapmles will give the same result: 200 configurations in trajectory .xyz file
 """
 
 from espresso.esutil import cxxinit
