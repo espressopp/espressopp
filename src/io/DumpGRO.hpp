fd7504b8219de4e259bb08264b83de2624853569
Konstantin Koschke <konstantin.koschke@gmail.com>
2013-10-24 15:37:58 +0200

    added atom type to output in DumpGRO

--- src/io/DumpGRO.hpp
+++ src/io/DumpGRO.hpp
@@ -2,9 +2,15 @@
 #ifndef _IO_DUMPGRO_HPP
 #define _IO_DUMPGRO_HPP
 
+#include "mpi.hpp"
+#include <boost/serialization/map.hpp>
+#include "types.hpp"
+#include "System.hpp"
 #include "io/FileBackup.hpp"
 #include "ParticleAccess.hpp"
 #include "integrator/MDIntegrator.hpp"
+#include "storage/Storage.hpp"
+#include "iterator/CellListIterator.hpp"
 
 #include "esutil/Error.hpp"
 
@@ -31,20 +37,38 @@ namespace espresso {
                         length_factor(_length_factor),
                         append(_append){ 
         setLengthUnit(_length_unit);
-        /*
-
-        shared_ptr<System> system = getSystem();
-        ConfigurationsExt conf( system );
-        conf.gather();
         
-        if( system->comm->rank()==0 ){
-          ConfigurationExtPtr conf_real = conf.back();
-          
-          int num_of_particles = conf_real->getSize();
-        }
-        particleIDToType.resize(num_of_particles);
 
-        */
+        
+        //if( system->comm->rank()==0 ){
+        //get local particle ID map
+        std::map<long, short> myParticleIDToTypeMap;
+        CellList realCells = system->storage->getRealCells();
+        for (iterator::CellListIterator cit(realCells); !cit.isDone(); ++cit) {
+          long id = cit->id();
+          short type = cit->type();
+          myParticleIDToTypeMap[id] = type;
+        }
+        if (myParticleIDToTypeMap.size() ==0 )
+          throw std::runtime_error("Dumper: No particles found in the system - make sure particles are added first before Dumper is initialized");
+        //std::cout << "my rank: " << system->comm->rank() << ", number of particles: " << myParticleIDToTypeMap.size() << std::endl;
+
+        //gather all particle ID maps
+        std::vector< std::map<long, short> > allParticleIDMaps;
+        boost::mpi::all_gather(
+            *getSystem()->comm,
+            myParticleIDToTypeMap, 
+            allParticleIDMaps);
+            
+        //merge all particle ID maps
+        //std::cout << "allParticleIDMaps.size(): " << allParticleIDMaps.size() << std::endl;
+        for (std::vector< std::map<long, short> >::iterator it=allParticleIDMaps.begin(); it!=allParticleIDMaps.end(); ++it)
+        {
+          particleIDToType.insert(it->begin(), it->end());
+        }
+        //std::cout << "particleIDToType.size(): " << particleIDToType.size() << std::endl;
+
+
         if( system->comm->rank()==0 && !append){
           FileBackup backup(file_name); //backup trajectory if it already exists
         }
@@ -94,7 +118,7 @@ namespace espresso {
 
       //an array or an map where key: particle id and value: particle type
       //we assume, that the type of a particle does not change over time
-      //std::vector<int> particleIDToType;
+      std::map<long, short> particleIDToType;
       
       bool unfolded;  // one can choose folded or unfolded coordinates, by default it is folded
       bool append; //append to existing trajectory file or create a new one
