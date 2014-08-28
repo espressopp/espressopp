874b1442b74176bad7ea46df379a7dc3f397eba6
Torsten Stuehn <stuehn@mpip-mainz.mpg.de>
2014-08-28 17:42:28 +0200

    increased Version to 1.9

--- src/Version.hpp
+++ src/Version.hpp
@@ -27,8 +27,8 @@
 #include "boost/version.hpp"
 
 #define MAJORVERSION 1
-#define MINORVERSION 8
-#define PATCHLEVEL   1
+#define MINORVERSION 9
+#define PATCHLEVEL   0
 #include "hgversion.hpp"
 
 namespace espresso {
