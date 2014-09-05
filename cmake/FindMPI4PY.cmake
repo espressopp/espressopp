1fccf7cb35528d58f3c9caccbf7ba94571adbae5
Christoph Junghans <junghans@votca.org>
2014-09-05 11:42:44 -0600

    cmake: clean FindMPI4PY.cmake (fix #2)

--- cmake/FindMPI4PY.cmake
+++ cmake/FindMPI4PY.cmake
@@ -19,19 +19,13 @@ endif(PYTHON_EXECUTABLE)
 if(PYTHON_EXECUTABLE)
   execute_process(COMMAND "${PYTHON_EXECUTABLE}" 
                 -c "import sys, mpi4py; sys.stdout.write(mpi4py.__version__)"
-                    OUTPUT_VARIABLE _MPI4PY_VERSION
+                    OUTPUT_VARIABLE MPI4PY_VERSION
                     RESULT_VARIABLE _MPI4PY_VERSION_RESULT
                     ERROR_QUIET)
   if(NOT _MPI4PY_VERSION_RESULT)
-        message("-- mpi4py version: " ${_MPI4PY_VERSION})
-        string(REPLACE "." ";" MPI4PY_VERSION_STRING "${_MPI4PY_VERSION}")
-        list(GET MPI4PY_VERSION_STRING 0 MPI4PY_VERSION_MAJOR)
-        list(GET MPI4PY_VERSION_STRING 1 MPI4PY_VERSION_MINOR)
-        list(GET MPI4PY_VERSION_STRING 2 MPI4PY_VERSION_PATCH)
-        if (${MPI4PY_VERSION_MAJOR} GREATER 0)
-          if (${MPI4PY_VERSION_MINOR} GREATER 2)
-            set(MPI4PY_VERSION_OK TRUE)
-          endif()
+        message("-- mpi4py version: " ${MPI4PY_VERSION})
+        if (${MPI4PY_FIND_VERSION} VERSION_LESS ${MPI4PY_VERSION})
+          set(MPI4PY_VERSION_OK TRUE)
         endif()
         if(NOT MPI4PY_VERSION_OK)
           message("     ESPResSo++ requires at least mpi4py version 1.3")
