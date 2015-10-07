8947d66538b2b6785170a8bcf763e3abbd050d34
Nikita Tretyakov <niktre@gmail.com>
2015-10-07 18:48:10 +0200

    Revert to a working version of LB. Added a parameter for profiling from the python script.

--- src/integrator/LatticeSite.hpp
+++ src/integrator/LatticeSite.hpp
@@ -30,16 +30,11 @@ namespace espressopp {
   namespace integrator {
 		class LBSite {
 			/**
-			 * \brief Description of the properties of the Site class
+			 * \brief Description of the properties of the LBSite class
 			 *
-			 * This is a Site class for the Lattice Boltzmann method. 
-			 * Everything that happens on the node is handled here:
-			 * - setting equilibrium weights and inveresed coefficients;
-			 * - calculation of local and (implicitly) equilibrium moments;
-			 * - relaxation of the moments to their equilibrium values;
-			 * - accounting for fluctuations (if desired); 
-			 * - application of external and coupling forces (if desired);
-			 * - back-transformation from the mode- to the population-space.
+			 * This is a LBSite class for storing of the populations, ext and coupling forces on the lattice site. Through its methods this class handles everything that happens on the node during collision. It also sets the values to the D3Q19 model-related parameters on EVERY lattice site.
+			 *
+			 * The normal lattice and its ghost counterpart are based on this LBSite class. They are defined in LatticeBoltzmann.*pp files.
 			 *
 			 * Please note that by default ESPResSo++ supports only D3Q19 lattice model.
 			 * However, you can code other lattice models, it should not be difficult.
@@ -51,12 +46,6 @@ namespace espressopp {
 			/* SET AND GET DECLARATION */
 			void setF_i (int _i, real _f);									// set f_i population to _f
 			real getF_i (int _i);														// get f_i population
-
-			void setInvBLoc (int _i, real _b);							// set invLov_b value to _b
-			real getInvBLoc (int _i);												// get invLoc_b value
-
-			void setEqWeightLoc (int _i, real _w);					// set eqWeightLoc value to _w
-			real getEqWeightLoc (int _i);										// get eqWeightLoc value
 
 			void setPhiLoc (int _i, real _phi);							// set phi value to _phi
 			real getPhiLoc (int _i);												// get phi value
@@ -73,62 +62,47 @@ namespace espressopp {
 			void setGammaEvenLoc (real _gamma_even);				// set gamma even
 			real getGammaEvenLoc ();												// get gamma even
 
-			void setExtForceLoc (Real3D _extForceLoc);			// set local external force
-			Real3D getExtForceLoc ();												// get local external force
-			
-			void setCouplForceLoc (Real3D _couplForceLoc);	// set local coupling force
-			Real3D getCouplForceLoc ();											// get local coupling force
-			
 			/* HELPFUL OPERATIONS WITH POPULATIONS AND MOMENTS */
 			void scaleF_i (int _i, real _value);						// scale population i by _value
-			void addExtForceLoc (Real3D _extForceLoc);			// add local external force
-			void addCouplForceLoc (Real3D _couplForceLoc);	// add local coupling force
-			
+
 			/* FUNCTIONS DECLARATION */
-			void initLatticeModelLoc ();										// local eq weights
+//			void initLatticeModelLoc ();										// local eq weights
 			void collision (int _lbTempFlag, int _extForceFlag,
-											int _couplForceFlag);						// perform collision step
+											int _couplForceFlag, Real3D _f);						// perform collision step
 			void calcLocalMoments (real *m);								// calculate local moments
 			void relaxMoments (real *m,
-												 int _extForceFlag);					// relax local moms to eq moms
+												 int _extForceFlag, Real3D _f);					// relax local moms to eq moms
 			void thermalFluct (real *m);										// apply thermal fluctuations
-			void applyForces (real *m);											// apply ext and coupl forces
+			void applyForces (real *m, Real3D _f);											// apply ext and coupl forces
 			void btranMomToPop (real *m);										// back-transform moms to pops
 
 		private:
 			std::vector<real> f;														// populations on a site
-			Real3D extForceLoc;															// local external force
-			Real3D couplForceLoc;														// local coupling force
 			static real gamma_bLoc;													// gamma bulk
 			static real gamma_sLoc;													// gamma shear
 			static real gamma_oddLoc;												// gamma odd
 			static real gamma_evenLoc;											// gamma even
-			static std::vector<real> eqWeightLoc;						// local eq. weights
-			static std::vector<real> inv_bLoc;							// local inverse coeff b_i
 			static std::vector<real> phiLoc;								// local fluct amplitudes
     };
 		
 /*******************************************************************************************/
 		
-    class GhostLattice {
+    class LBMom {
 			/**
-			 * \brief Description of the properties of the GhostLattice class
+			 * \brief Description of the properties of the LBMom class
 			 *
-			 * This is a GhostLattice class for storing of the populations from Site class while 
-			 * streaming. It is a handy yet not necessary procedure. There is a possibility that 
-			 * in the future we will dispose of this class and implement streaming with memory 
-			 * moves. However, at the moment we aim at the code that can be well understood by a 
-			 * non-expert and this class is a must!
+			 * This is a LBMom class for storing of the hydrodynamic moments on the lattice site. 
+			 * These include density and 3-comp. mass flux.
 			 */
 		public:
-			GhostLattice ();																// constr of the ghost lattice
-			~GhostLattice ();																// destr of the ghost lattice
+			LBMom ();																// constr of the ghost lattice
+			~LBMom ();																// destr of the ghost lattice
 
-			void setPop_i (int _i, real _pop);							// set f_i population to _f
-			real getPop_i (int _i);													// get f_i population
+			void setMom_i (int _i, real _mom);							// set f_i population to _f
+			real getMom_i (int _i);													// get f_i population
 			
 		private:
-			std::vector<real> pop;													// pops on the ghost lattice
+			std::vector<real> mom;													// pops on the ghost lattice
     };
 		
 /*******************************************************************************************/
@@ -146,11 +120,41 @@ namespace espressopp {
 			static void setTauLoc (real _tauLoc);						// set tauLocal
 			static real getTauLoc ();												// get tauLocal
 
+			static void setEqWeightLoc (int _i, real _w);		// set eqWeightLoc value to _w
+			static real getEqWeightLoc (int _i);						// get eqWeightLoc value
+
+			static void setInvBLoc (int _i, real _b);				// set invLov_b value to _b
+			static real getInvBLoc (int _i);								// get invLoc_b value
+			
+			void initEqWeights();
+			void initInvBLoc();
+			
 			static shared_ptr< esutil::RNG > rng;						//!< RNG for fluctuations
 		private:
 			static int numVelsLoc;													// local number of vels
 			static real aLoc;																// local lattice spacing
 			static real tauLoc;															// local time spacing
+			static std::vector<real> eqWeightLoc;						// local eq. weights
+			static std::vector<real> inv_bLoc;							// local inverse coeff b_i
+		};
+		
+		class LBForce {
+		public:
+			LBForce ();
+			~LBForce ();
+
+			void setExtForceLoc (Real3D _extForceLoc);			// set local external force
+			Real3D getExtForceLoc ();												// get local external force
+			
+			void setCouplForceLoc (Real3D _couplForceLoc);	// set local coupling force
+			Real3D getCouplForceLoc ();											// get local coupling force
+			
+			void addExtForceLoc (Real3D _extForceLoc);			// add local external force
+			void addCouplForceLoc (Real3D _couplForceLoc);	// add local coupling force
+			
+		private:
+			Real3D extForceLoc;															// local external force
+			Real3D couplForceLoc;														// local coupling force
 		};
   }
 }
