a051261a04cbb7d242a573e81a4f3009b7d8a34e
Jakub Krajniak <jkrajniak@gmail.com>
2015-12-24 19:41:59 +0100

    added additional test to check if original API works correctly

--- testsuite/system_test/system_test.py
+++ testsuite/system_test/system_test.py
@@ -20,10 +20,12 @@ vl = espressopp.VerletList(system, cutoff=1.4)
 lj1 = espressopp.interaction.VerletListLennardJones(vl)
 lj2 = espressopp.interaction.VerletListLennardJones(vl)
 lj3 = espressopp.interaction.VerletListLennardJones(vl)
+lj4 = espressopp.interaction.VerletListLennardJones(vl)
 
 system.addInteraction(lj1, 'lj1')
 system.addInteraction(lj2, 'lj2')
 system.addInteraction(lj3, 'lj3')
+system.addInteraction(lj4)
 
 # Checks if all interactions are present.
 assert set(['lj1', 'lj2', 'lj3']) == set(system.getAllInteractions().keys())
@@ -34,9 +36,9 @@ assert id(lj2) == id(system.getInteraction(1))
 assert id(lj3) == id(system.getInteraction(2))
 
 # Checks remove by name.
-assert system.getNumberOfInteractions() == 3
+assert system.getNumberOfInteractions() == 4
 system.removeInteractionByName('lj2')
-assert system.getNumberOfInteractions() == 2
+assert system.getNumberOfInteractions() == 3
 
 # Checks if interactions are renumbered correctly.
 assert id(lj1) == id(system.getInteractionByName('lj1'))
