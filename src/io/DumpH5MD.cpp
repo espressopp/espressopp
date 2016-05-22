cbe922ee4f7f98ebedf2063aaf84fb026414e272
Jakub Krajniak <jkrajniak@gmail.com>
2016-05-22 09:47:48 +0200

    added timers to measure times in DumpH5MD; store static data with DumpTopology.

--- src/io/DumpH5MD.cpp
+++ src/io/DumpH5MD.cpp
@@ -2,7 +2,7 @@
   Copyright (c) 2015
       Pierre de Buyl
 
-  Copyright (c) 2015-2016
+  Copyright (c) 2015,2016
       Jakub Krajniak (jkrajniak at gmail.com)
 
   This file is part of ESPResSo++.
