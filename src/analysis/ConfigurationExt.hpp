fb17d31fe12d6e5a7390b66064ba5624f51a192c
Federico G. Padua <padua@uni-mainz.de>
2016-09-25 02:18:37 +0200

    Reopen #32 that was closed due to developer Github account refactoring.
    Small and minimal extensions to DumpXYZ. Now can write standard and
    non standard XYZ files (e.g. as fastwritexyz). 
    Printing of particle type is not hard coded but 
    retrieved and one can give the option to store pid
    and/or velocities (default set to standard XYZ format).
    Precision for printing doubles as fastwritexyx but 
    can be changed. Changed fastwritexyz
    and others in DumpConfigurations.py accordingly.

--- src/analysis/ConfigurationExt.hpp
+++ src/analysis/ConfigurationExt.hpp
@@ -3,21 +3,21 @@
       Max Planck Institute for Polymer Research
   Copyright (C) 2008,2009,2010,2011
       Max-Planck-Institute for Polymer Research & Fraunhofer SCAI
-  
+
   This file is part of ESPResSo++.
-  
+
   ESPResSo++ is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   ESPResSo++ is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // ESPP_CLASS
@@ -42,9 +42,14 @@ namespace espressopp {
       ConfigurationExtIterator(std::map<size_t, RealND >& coordinates);
       //ConfigurationExtIterator(std::map<size_t, Real3D>& coordinates);
 
+      size_t currentId();
+      RealND currentProperties();
+
+      void incrementIterator();
+
       /** Get next particle id for which properties are available */
 
-      int nextId();
+      size_t nextId();
       const RealND nextProperties();
       //Real3D nextCoordinates();
       //Real3D nextVelocities();
