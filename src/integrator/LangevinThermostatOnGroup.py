6b09e7f53a1e6ddc3d7e6567b702800f012f34ef
Jakub Krajniak <jkrajniak@gmail.com>
2016-10-01 10:50:26 +0200

    removed trailing spaces

--- src/integrator/LangevinThermostatOnGroup.py
+++ src/integrator/LangevinThermostatOnGroup.py
@@ -1,20 +1,20 @@
 #  Copyright (C) 2016
 #      Jakub Krajniak (jkrajniak at gmail.com)
-#  
+#
 #  This file is part of ESPResSo++.
-#  
+#
 #  ESPResSo++ is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  ESPResSo++ is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 r"""
@@ -26,10 +26,10 @@ Thermalize particles in the ParticleGroup only.
 
 .. function:: espressopp.integrator.LangevinThermostatOnGroup(system, particle_group)
 
-		:param system: The system object.
-		:type system: espressopp.System
-		:param particle_group: The particle group.
-		:type particle_group: espressopp.ParticleGroup
+        :param system: The system object.
+        :type system: espressopp.System
+        :param particle_group: The particle group.
+        :type particle_group: espressopp.ParticleGroup
 """
 from espressopp.esutil import cxxinit
 from espressopp import pmi
