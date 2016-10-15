7575ca7ff8ca94201384ebf3a3b35986712d2997
Nikita Tretyakov <niktre@gmail.com>
2016-10-15 13:22:32 +0200

    Fixed LB site indices when profiling Vz (x) on multi-core; calculation of den and jz --> getting their values

--- src/analysis/LBOutputVzOfX.cpp
+++ src/analysis/LBOutputVzOfX.cpp
@@ -24,44 +24,35 @@
 
 namespace espressopp {
    namespace analysis {
-      //    LOG4ESPP_LOGGER(LBOutputVzOfX::theLogger, "LBOutputVzOfX");
       LBOutputVzOfX::LBOutputVzOfX(shared_ptr<System> system,
                                    shared_ptr< integrator::LatticeBoltzmann > latticeboltzmann)
       : LBOutput(system, latticeboltzmann) {}
       
       void LBOutputVzOfX::writeOutput()
       {
-         Int3D _Ni = latticeboltzmann->getMyNi();
-         int _numVels = latticeboltzmann->getNumVels();
          int _step = latticeboltzmann->getStepNum();
+         int _offset = latticeboltzmann->getHaloSkin();
+         int _j = _offset, _k = _offset;
+         Int3D _Ni = latticeboltzmann->getMyNi();
+         Int3D _globIdx = latticeboltzmann->findGlobIdx();// first lb site global index
          
+         // print profiling notification //
+         if (mpiWorld->rank() == 0 && _step == 0)
+            printf("LBOutputVzOfX: Making velocity profile v_z (x)\n\n");
          
-         std::vector<real> _denLoc = std::vector<real>(_Ni.getItem(0), 0.);
-         std::vector<real> _jzLoc = std::vector<real>(_Ni.getItem(0), 0.);
-
-         if (_step == 0) printf("LBOutputVzOfX: Making velocity profile v_z (x)\n\n");
-         
-         /* interface to create filename for the output profile */
+         // construct filename for the output profile //
          std::string filename;
          std::ostringstream convert;
          convert << _step;
-         
          filename = "vz_of_x."; filename.append(convert.str()); filename.append(".dat");
          
-         /* define lattice sites to measure velocity profile. We use a simple profile Vz(x)
-          * at y=0 and z=0 (indexes _j and _k are set to offset) */
-
-         int _offset = latticeboltzmann->getHaloSkin();
-         int _j = _offset, _k = _offset;
-
-         // creating a profile based on the current populations
+         // output profile Vz(x)|_{y=z=0} (indices _j and _k set to offset) //
          FILE * velProfFile = fopen(filename.c_str(),"a");
+         
          for (int i = _offset; i < _Ni[0] - _offset; i++) {
-            for (int l = 0; l < _numVels; l++) {
-               _denLoc[i] += latticeboltzmann->getPops( Int3D(i,_j,_k), l );
-               _jzLoc[i] += latticeboltzmann->getPops( Int3D(i,_j,_k), l ) * latticeboltzmann->getCi(l).getItem(2);
-            }
-            fprintf (velProfFile, "%9d %9.6f %9.6f  \n", i, _denLoc[i], _jzLoc[i]/_denLoc[i]);
+            real _den = latticeboltzmann->getLBMom( Int3D(i,_j,_k), 0 );
+            real _jz  = latticeboltzmann->getLBMom( Int3D(i,_j,_k), 3 );
+            fprintf (velProfFile, "%9d %9.6f %9.6f  \n", _globIdx[0] + i, _den, _jz / _den);
          }
          
          fclose (velProfFile);
@@ -79,4 +70,3 @@ namespace espressopp {
       }
    }
 }
-
