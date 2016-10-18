e32d0352e56dad94078e21d3b4534cc4856e2eb6
Hideki Kobayashi <kobayashih@pckr168.mpip-mainz.mpg.de>
2016-10-18 16:06:49 +0200

    Fixed potential formula

--- src/interaction/VSpherePair.hpp
+++ src/interaction/VSpherePair.hpp
@@ -37,10 +37,10 @@ namespace espressopp {
 
     \f[
 
-         V(r_ij, \sigma_ij) = \frac{\varepsilon}{\beta} \left( \frac{2 \pi}{3} \right)
-                              \sigma_ij^{- \frac{3}{2}} e^{- \frac{3}{2} \frac{r_ij}{\sigma_ij}} ,
-                              r_ij = \left| \vec{r_i} - \vec{r_j} \right| ,
-                              \sigma_ij = \sigma_i^2 + \sigma_j^2
+         V(r_{ij}, \sigma_{ij}) = \frac{\varepsilon}{\beta} \left( \frac{2 \pi}{3} \right)
+                              \sigma_ij^{- \frac{3}{2}} e^{- \frac{3}{2} \frac{r_{ij}^2}{\sigma_{ij}}} ,
+                              r_{ij} = \left| \vec{r_i} - \vec{r_j} \right| ,
+                              \sigma_{ij} = \sigma_i^2 + \sigma_j^2
 
     \f]
 
@@ -80,8 +80,8 @@ namespace espressopp {
       virtual ~VSpherePair() {};
 
       void preset() {
-      	mfh = -(3.0/2.0);
-    	mth = -(5.0/2.0);
+      	mth = -(3.0/2.0);
+    	mfh = -(5.0/2.0);
         ef1 = epsilon*pow(1.0L*(2*M_PIl/3.0), 1.0L*mth);
         ff1 = 3 * ef1;
       }
@@ -96,8 +96,7 @@ namespace espressopp {
       real getEpsilon() const { return epsilon; }
 
       real _computeEnergySqrRaw(real distSqr, real sigmaij) const {
-    	real rij = sqrt(distSqr);
-        real energy = ef1*pow(1.0L*sigmaij, 1.0L*mth)*exp(mth*rij/sigmaij);
+        real energy = ef1*pow(1.0L*sigmaij, 1.0L*mth)*exp(mth*distSqr/sigmaij);
         return energy;
       }
 
@@ -106,11 +105,9 @@ namespace espressopp {
                             real distSqr, real sigmai, real sigmaj) const {
 
     	real sigmaij = sigmai*sigmai + sigmaj*sigmaj;
-    	real rij     = sqrt(distSqr);
-    	real invrij  = 1/rij;
-    	real eh      = exp(mth*rij/sigmaij);
-    	real fs      = ff1*pow(1.0L*sigmaij, 1.0L*mfh) * eh * (1 + rij);
-    	force        = ff1*pow(1.0L*sigmaij, 1.0L*mth) * eh * dist * invrij;
+    	real eh      = exp(mth*distSqr/sigmaij);
+    	real fs      = ff1*pow(1.0L*sigmaij, 1.0L*mfh) * eh * (1 - 0.5*distSqr/sigmaij);
+    	force        = ff1*pow(1.0L*sigmaij, 1.0L*mfh) * eh * dist;
     	fsi          = fs * sigmai;
     	fsj          = fs * sigmaj;
     	return true;
