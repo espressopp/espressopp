a129f0e2630dd3c79582a7c312853bcb15d9cb6e
Nikita Tretyakov <niktre@gmail.com>
2017-02-06 18:14:10 +0100

    enlarge group logo to 400 px

--- doc/ug/conf.py
+++ doc/ug/conf.py
@@ -136,7 +136,7 @@ html_title = '{} v{}'.format(project, version)
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/logo_theory_group_200x48.png'
+html_logo = '_static/logo_theory_group.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
