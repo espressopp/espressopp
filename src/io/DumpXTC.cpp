bdd8a8bfb2e6e64dc2e104f5db47c67aad35ed47
Gregor Deichmann <deichmann@cpc.tu-darmstadt.de>
2017-04-10 13:28:51 +0200

    added xtc dump and corresponding unit test

---
+++ src/io/DumpXTC.cpp
@@ -0,0 +1,226 @@
+/*
+  Copyright (C) 2012,2013
+      Max Planck Institute for Polymer Research
+  Copyright (C) 2008,2009,2010,2011
+      Max-Planck-Institute for Polymer Research & Fraunhofer SCAI
+  
+  This file is part of ESPResSo++.
+  
+  ESPResSo++ is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  ESPResSo++ is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+#include <fstream>
+#include <iomanip>
+#include "DumpXTC.hpp"
+#include "storage/Storage.hpp"
+
+#include "bc/BC.hpp"
+
+#include "analysis/ConfigurationExt.hpp"
+#include "analysis/ConfigurationsExt.hpp"
+
+extern "C"{
+#include "libxdrf.c" //c-routines for xdr io and compression
+}
+
+using namespace espressopp;
+using namespace espressopp::analysis;
+using namespace std;
+
+namespace espressopp {
+  namespace io {
+    
+    bool DumpXTC::open(const char *mode){
+      
+      fp = fopen(file_name.c_str(),mode);
+      
+      if(!fp){
+        return false;
+      }
+      
+      xdrstdio_create(xdr,fp,xdrmode);
+      
+      return true;
+    }
+
+    void DumpXTC::close(){
+      
+      if(!fp){
+        return;
+      }
+      
+      if(xdr){
+        xdr_destroy(xdr);
+      }
+      
+      if(fp){
+        fclose(fp);
+        fp=NULL;
+      }   
+      
+      return;
+
+    }
+
+    bool DumpXTC::write(int natoms,  
+                              int step,
+                              float time,
+                              Real3D *box,
+                              float *x,
+                              float prec){
+
+        bool bOk = true;
+
+        int num=XTC_MAGIC,ival; //gromacs magic number (should be 1995)
+        float fval;
+        int i,j;
+
+        if( xdr_int(xdr,&num) == 0){
+            bOk = false;
+            return 1;
+        }
+
+        ival=natoms;
+        if( xdr_int(xdr,&ival) == 0){
+            bOk = false;
+            return 1;
+        }
+
+        ival=step;
+        if( xdr_int(xdr,&ival) == 0){
+            bOk = false;
+            return 1;
+        };
+
+        fval=time;
+        if( xdr_float(xdr,&fval) == 0){
+            bOk = false;
+            return 1;
+        };
+
+        for(i=0;i<dim;i++){
+            for(j=0;j<dim;j++){
+                fval = box[i][j];
+
+                if( xdr_float(xdr,&fval) == 0){
+                    bOk = false;
+                    break;
+                }
+            }
+            if(!bOk){
+                break;
+            }
+        }
+
+        if(!bOk){
+            return 1;
+        }
+
+        const int x_size = natoms*dim;
+        
+        if( xdr3dfcoord(xdr, x, &natoms, &prec) == 0 ){
+            bOk=false;
+        }
+
+        return bOk;
+    }
+
+
+      
+    void DumpXTC::dump(){
+
+      shared_ptr<System> system = getSystem();
+      ConfigurationsExt conf( system );
+      conf.setUnfolded(unfolded);
+      conf.gather();
+      
+      if( system->comm->rank()==0 ){
+        ConfigurationExtPtr conf_real = conf.back();
+        
+        int num_of_particles = conf_real->getSize();
+       
+        if(this->open("a")){ 
+        
+          ConfigurationExtIterator cei = conf_real-> getIterator();
+          //RealND::iterator ii;
+          //short ind;
+          Real3D *box = new Real3D [dim];
+          float *coord = new float [dim*num_of_particles];
+          RealND props;
+          props.setDimension( cei.currentProperties().getDimension() );
+
+          //HACK: Only valid for orthorhombic BC. Will there be anything else in ESPP?
+          Real3D bl=system->bc->getBoxL();
+
+          for(int i=0;i<dim;i++){
+            box[i][0] = 0.;
+            box[i][1] = 0.;
+            box[i][2] = 0.;
+
+            box[i][i] = bl[i]; 
+          }
+
+          for(int i=0;i<num_of_particles;i++){
+            
+            props=cei.nextProperties();
+            coord[i*dim] = props[0]*length_factor; //We only write coordinates to .xtc 
+            coord[i*dim+1] = props[1]*length_factor;
+            coord[i*dim+2] = props[2]*length_factor;
+
+          }
+
+          int step = integrator->getStep();
+          float time = integrator->getTimeStep()*step;
+
+          this->write(num_of_particles,step,time,box,coord,xtcprec);
+
+          delete [] box;
+          delete [] coord;
+          
+          this->close();
+        }
+        else cout << "Unable to open file: "<< file_name <<endl;
+
+      
+      }
+    }
+      
+    // Python wrapping
+    void DumpXTC::registerPython() {
+
+      using namespace espressopp::python;
+
+      class_<DumpXTC, bases<ParticleAccess>, boost::noncopyable >
+      ("io_DumpXTC", init< shared_ptr< System >, 
+                           shared_ptr< integrator::MDIntegrator >, 
+                           std::string, 
+                           bool,
+                           real,
+                           std::string,
+                           bool>())
+        .add_property("filename", &DumpXTC::getFilename, 
+                                  &DumpXTC::setFilename)
+        .add_property("unfolded", &DumpXTC::getUnfolded, 
+                                  &DumpXTC::setUnfolded)
+        .add_property("append", &DumpXTC::getAppend, 
+                                  &DumpXTC::setAppend)
+        .add_property("length_factor", &DumpXTC::getLengthFactor, 
+                                       &DumpXTC::setLengthFactor)
+        .add_property("length_unit", &DumpXTC::getLengthUnit, 
+                                     &DumpXTC::setLengthUnit)
+        .def("dump", &DumpXTC::dump)
+      ;
+    }
+  }
+}
---
+++ src/io/DumpXTC.py
@@ -0,0 +1,120 @@
+#  Copyright (C) 2012,2013
+#      Max Planck Institute for Polymer Research
+#  Copyright (C) 2008,2009,2010,2011
+#      Max-Planck-Institute for Polymer Research & Fraunhofer SCAI
+#
+#  This file is part of ESPResSo++.
+#
+#  ESPResSo++ is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ESPResSo++ is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+r"""
+*********************************************
+**DumpXTC** - IO Object
+*********************************************
+
+* `dump()`
+  write configuration to trajectory file in XTC format (Gromacs/Gromos compressed coordinate trajectory). By default filename is "out.xtc",
+  coordinates are folded.
+
+  Properties
+
+* `filename`
+  Name of trajectory file. By default trajectory file name is "out.xtc"
+
+* `unfolded`
+  False if coordinates are folded, True if unfolded. By default - False
+
+* `append`
+  True if new trajectory data is appended to existing trajectory file. By default - True
+
+* `length_factor`
+  If length dimension in current system is nm, and unit is 0.23 nm, for example, then
+  length_factor should be 0.23
+
+* `length_unit`
+  It is length unit. Can be LJ, nm or A. By default - LJ
+
+usage:
+
+writing down trajectory
+
+>>> dump_conf_xtc = espressopp.io.DumpXTC(system, integrator, filename='trajectory.xtc')
+>>> for i in range (200):
+>>>   integrator.run(10)
+>>>   dump_conf_gro.dump()
+
+writing down trajectory using ExtAnalyze extension
+
+>>> dump_conf_xtc = espressopp.io.DumpXTC(system, integrator, filename='trajectory.gro')
+>>> ext_analyze = espressopp.integrator.ExtAnalyze(dump_conf_gro, 10)
+>>> integrator.addExtension(ext_analyze)
+>>> integrator.run(2000)
+
+Both exapmles will give the same result: 200 configurations in trajectory .gro file.
+
+setting up length scale
+
+For example, the Lennard-Jones model for liquid argon with :math:`\sigma=0.34 [nm]`
+
+>>> dump_conf_xtc = espressopp.io.DumpXTC(system, integrator, filename='trj.xtc', unfolded=False, length_factor=0.34, length_unit='nm', append=True)
+
+will produce trj.gro with in nanometers
+
+.. function:: espressopp.io.DumpXTC(system, integrator, filename, unfolded, length_factor, length_unit, append)
+
+		:param system:
+		:param integrator:
+		:param filename: (default: 'out.gro')
+		:param unfolded: (default: False)
+		:param length_factor: (default: 1.0)
+		:param length_unit: (default: 'LJ')
+		:param append: (default: True)
+		:type system:
+		:type integrator:
+		:type filename:
+		:type unfolded:
+		:type length_factor: real
+		:type length_unit:
+		:type append:
+
+.. function:: espressopp.io.DumpXTC.dump()
+
+		:rtype:
+"""
+
+from espressopp.esutil import cxxinit
+from espressopp import pmi
+
+from espressopp.ParticleAccess import *
+from _espressopp import io_DumpXTC
+
+class DumpXTCLocal(ParticleAccessLocal, io_DumpXTC):
+
+  def __init__(self, system, integrator, filename='out.xtc', unfolded=False, length_factor=1.0, length_unit='LJ', append=True):
+    cxxinit(self, io_DumpXTC, system, integrator, filename, unfolded, length_factor, length_unit, append)
+
+  def dump(self):
+    if not pmi._PMIComm or pmi._MPIcomm.rank in pmi._PMIComm.getMPIcpugroup():
+      self.cxxclass.dump(self)
+
+
+if pmi.isController :
+  class DumpXTC(ParticleAccess):
+    __metaclass__ = pmi.Proxy
+    pmiproxydefs = dict(
+      cls =  'espressopp.io.DumpXTCLocal',
+      pmicall = [ 'dump' ],
+      pmiproperty = ['filename', 'unfolded', 'length_factor', 'length_unit', 'append']
+    )
