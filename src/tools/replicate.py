6d0cce32d2a188a42c05903ec9e5444dba427d1e
James Vance <vance@uni-mainz.de>
2022-03-22 18:30:09 +0100

    Moved parallel implementation below

--- src/tools/replicate.py
+++ src/tools/replicate.py
@@ -23,33 +23,62 @@
 from espressopp import pmi
 from espressopp import Real3D
 
-class ReplicateParallelLocal:
-    """ Usage:
+def replicate (bonds, angles, x, y, z, Lx, Ly, Lz, xdim=1, ydim=1, zdim=1):
+    """
+    Replicates configuration in each dimension.
 
-        replicate   = (2,2,2)
-        bonds, angles, x, y, z, Lx, Ly, Lz = espressopp.tools.lammps.read('polymer_melt.lammps')
-        rp = espressopp.tools.ReplicateParallel()
-        num_particles, Lx, Ly, Lz = rp.replicate(bonds, angles, x, y, z, Lx, Ly, Lz, *replicate)
-        box = (Lx, Ly, Lz)
-        system, integrator = espressopp.standard_system.Default(box=box,...)
+    This may be used to increase the size of an equilibrated melt by a factor of 8 or more.
 
-        props = ['type', 'mass']
-        num_particles_seed = len(x)
-        seed_particles = []
-        for i in range(num_particles_seed):
-            part = [0, 1.0]
-            seed_particles.append(part)
+    Presently this routine works only for semiflexible polymers. A general
+    class should be written to deal with files containing coordinates
+    and topology data.
 
-        rp.addParticles(system.storage, 1, seed_particles, *props)
-        system.storage.decompose()
-
-        fpl = espressopp.FixedPairList(system.storage)
-        rp.addBonds(fpl)
-
-        ftl = espressopp.FixedTripleList(system.storage)
-        rp.addTriples(ftl)
+    xdim = ydim = zdim = 1 returns the original system not replicated.
+    xdim = ydim = zdim = 2 returns the original system replicated to 8x.
+    xdim = ydim = zdim = 3 returns the original system replicated to 27x.
+    xdim = ydim = 1, zdim = 2 returns the original system replicated in the z-direction.
     """
 
+    # replicate the particles
+    x_replicated = x[:]
+    y_replicated = y[:]
+    z_replicated = z[:]
+    for i in range(xdim):
+        for j in range(ydim):
+            for k in range(zdim):
+                if(i + j + k != 0):
+                    for x_, y_, z_ in zip(x, y, z):
+                        x_replicated.append(x_ + i * Lx)
+                        y_replicated.append(y_ + j * Ly)
+                        z_replicated.append(z_ + k * Lz)
+
+    # replicate the bonds and angles
+    ct = 0
+    num_particles_original = len(x)
+    bonds_replicated = bonds[:]
+    angles_replicated = angles[:]
+    for i in range(xdim):
+        for j in range(ydim):
+            for k in range(zdim):
+                if(i + j + k != 0):
+                    ct = ct + 1
+                    for p1, p2 in bonds:
+                        bonds_replicated.append((p1 + ct * num_particles_original, \
+                                                 p2 + ct * num_particles_original))
+                    for p1, p2, p3 in angles:
+                        angles_replicated.append((p1 + ct * num_particles_original, \
+                                                  p2 + ct * num_particles_original, \
+                                                  p3 + ct * num_particles_original))
+
+    # modify the box size
+    Lx = xdim * Lx
+    Ly = ydim * Ly
+    Lz = zdim * Lz
+
+    return bonds_replicated, angles_replicated, x_replicated, y_replicated, z_replicated, Lx, Ly, Lz
+
+
+class ReplicateParallelLocal:
     def __init__(self):
         pass
 
@@ -113,6 +142,37 @@ class ReplicateParallelLocal:
 
 if pmi.isController:
     class ReplicateParallel(metaclass=pmi.Proxy):
+        """
+        Performs the replicate operation in parallel on all workers and creates the replicated
+        particles, bonds and angles only when they are being added to the respective classes.
+        They are also instantiated by batches of the same size as the original seed so memory
+        footprint is reduced.
+
+        Usage:
+
+            replicate   = (2,2,2)
+            bonds, angles, x, y, z, Lx, Ly, Lz = espressopp.tools.lammps.read('polymer_melt.lammps')
+            rp = espressopp.tools.ReplicateParallel()
+            num_particles, Lx, Ly, Lz = rp.replicate(bonds, angles, x, y, z, Lx, Ly, Lz, *replicate)
+            box = (Lx, Ly, Lz)
+            system, integrator = espressopp.standard_system.Default(box=box,...)
+            ...
+            props = ['type', 'mass']
+            num_particles_seed = len(x)
+            seed_particles = []
+            for i in range(num_particles_seed):
+                part = [0, 1.0]
+                seed_particles.append(part)
+            rp.addParticles(system.storage, 1, seed_particles, *props)
+            system.storage.decompose()
+            ...
+            fpl = espressopp.FixedPairList(system.storage)
+            rp.addBonds(fpl)
+            ...
+            ftl = espressopp.FixedTripleList(system.storage)
+            rp.addTriples(ftl)
+        """
+
         pmiproxydefs = dict(
           cls = 'espressopp.tools.ReplicateParallelLocal',
           pmicall = ['replicateWorker', 'addParticlesWorker', 'addBonds', 'addTriples']
@@ -132,57 +192,3 @@ if pmi.isController:
             if 'id' in properties:
                 raise ValueError("properties should not include 'id'")
             self.addParticlesWorker(storage, start_pid, seed_particles, properties)
-
-def replicate (bonds, angles, x, y, z, Lx, Ly, Lz, xdim=1, ydim=1, zdim=1):
-    """
-    Replicates configuration in each dimension.
-
-    This may be used to increase the size of an equilibrated melt by a factor of 8 or more.
-
-    Presently this routine works only for semiflexible polymers. A general
-    class should be written to deal with files containing coordinates
-    and topology data.
-
-    xdim = ydim = zdim = 1 returns the original system not replicated.
-    xdim = ydim = zdim = 2 returns the original system replicated to 8x.
-    xdim = ydim = zdim = 3 returns the original system replicated to 27x.
-    xdim = ydim = 1, zdim = 2 returns the original system replicated in the z-direction.
-    """
-
-    # replicate the particles
-    x_replicated = x[:]
-    y_replicated = y[:]
-    z_replicated = z[:]
-    for i in range(xdim):
-        for j in range(ydim):
-            for k in range(zdim):
-                if(i + j + k != 0):
-                    for x_, y_, z_ in zip(x, y, z):
-                        x_replicated.append(x_ + i * Lx)
-                        y_replicated.append(y_ + j * Ly)
-                        z_replicated.append(z_ + k * Lz)
-
-    # replicate the bonds and angles
-    ct = 0
-    num_particles_original = len(x)
-    bonds_replicated = bonds[:]
-    angles_replicated = angles[:]
-    for i in range(xdim):
-        for j in range(ydim):
-            for k in range(zdim):
-                if(i + j + k != 0):
-                    ct = ct + 1
-                    for p1, p2 in bonds:
-                        bonds_replicated.append((p1 + ct * num_particles_original, \
-                                                 p2 + ct * num_particles_original))
-                    for p1, p2, p3 in angles:
-                        angles_replicated.append((p1 + ct * num_particles_original, \
-                                                  p2 + ct * num_particles_original, \
-                                                  p3 + ct * num_particles_original))
-
-    # modify the box size
-    Lx = xdim * Lx
-    Ly = ydim * Ly
-    Lz = zdim * Lz
-
-    return bonds_replicated, angles_replicated, x_replicated, y_replicated, z_replicated, Lx, Ly, Lz
