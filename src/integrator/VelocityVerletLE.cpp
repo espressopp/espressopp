c19cad00a08b31d608dc381f24f714e65545476a
xzhh <zhexu@uni-mainz.de>
2022-07-21 14:50:01 +0200

    Add SLLOD equations for LEBC

--- src/integrator/VelocityVerletLE.cpp
+++ src/integrator/VelocityVerletLE.cpp
@@ -348,10 +348,12 @@ real VelocityVerletLE::integrate1()
 
         real dtfm = 0.5 * dt / cit->mass();
 
-        // Propagate velocities for X dim.
-        cit->velocity()[0] += dtfm * cit->force()[0];
-        real vshear = shearRate * (cit->position()[2] + 0.5 * cit->velocity()[2] * dt +
-                                   dtfm * cit->force()[2] * dt / 3.0 - halfL);
+        // Propagate velocities for X dim (SLLOD).
+        cit->velocity()[0] += dtfm * cit->force()[0] - 0.5 * dt * cit->velocity()[2] * shearRate; // With-SLLOD
+        // cit->velocity()[0] += dtfm * cit->force()[0]; // Non-SLLOD
+        real vshear = shearRate * (cit->position()[2] - halfL);
+                                   // + 0.5 * cit->velocity()[2] * dt // first
+                                   // + dtfm * cit->force()[2] * dt / 3.0); // and second order for coord propagation
         // Propagate velocities for Y-Z dim.: v(t+0.5*dt) = v(t) + 0.5*dt * f(t)
         cit->velocity()[2] += dtfm * cit->force()[2];
         cit->velocity()[1] += dtfm * cit->force()[1];
@@ -426,6 +428,8 @@ void VelocityVerletLE::integrate2()
             real dtfm = half_dt / cit->mass();
             /* Propagate velocities: v(t+0.5*dt) = v(t) + 0.5*dt * f(t) */
             cit->velocity() += dtfm * cit->force();
+            // SLLOD correction
+            cit->velocity()[0] -= half_dt * cit->velocity()[2] * shearRate; // With-SLLOD
             // Need to add propagation of shear speed if necessary
             // Collect xz-&zx- components from stress Tensor
             mv2 += cit->mass() * cit->velocity()[0] * cit->velocity()[2];
@@ -450,6 +454,8 @@ void VelocityVerletLE::integrate2()
             real dtfm = half_dt / cit->mass();
             /* Propagate velocities: v(t+0.5*dt) = v(t) + 0.5*dt * f(t) */
             cit->velocity() += dtfm * cit->force();
+            // SLLOD correction
+            cit->velocity()[0] -= half_dt * cit->velocity()[2] * shearRate; // With-SLLOD
         }
     }
 
